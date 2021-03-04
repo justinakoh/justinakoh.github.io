a43c5487f38b8023ab1942fe43a85af121d15c9d
justinakoh <justina.y.koh@gmail.com>
2021-03-04 14:32:56 +1300

    Update code to have proper styling

--- src/App.js
+++ src/App.js
@@ -15,17 +15,19 @@ function App() {
                         I'm a software engineer who is passionate about creating beautiful software that users love to
                         use, and developers love to maintain.
                         <br/><br/>
-                        Why? Because there is nothing worse than old, outdated software which is hard to use, hard to read,
+                        Why? Because there is nothing worse than old, outdated software which is hard to use, hard to
+                        read,
                         and hard to maintain.
                     </p>
                 </div>
                 <div className="subtext">
-                    <p>Find me on </p>
-                    <a href="https://github.com/justinakoh" target="_blank" className="body-link twitter-link"
-                       unselectable="on">Github</a>
-                    <p>or Linkedin</p>
-                    <a href="https://nz.linkedin.com/in/justina-koh" target="_blank" className="body-link twitter-link"
-                       unselectable="on">Linkedin</a>
+                    <p>Find me on &nbsp;
+                        <a href="https://github.com/justinakoh" target="_blank" className="body-link"
+                           unselectable="on">Github &nbsp;</a>
+                        or &nbsp;
+                        <a href="https://nz.linkedin.com/in/justina-koh" target="_blank" className="body-link"
+                           unselectable="on">Linkedin</a>
+                    </p>
                 </div>
             </div>
             <div className="projects">
@@ -36,31 +38,33 @@ function App() {
                 <p>
                     Cybersafe
                 </p>
-                <p>
+                <p className="name subtext">
                     An app created to help increase awareness about cybersecurity and safety online. Feeling brave?
                     Click on the link to see what you can see about your tech online.
                 </p>
                 <p>
                     Organise Me
                 </p>
-                <p>
+                <p className="name subtext">
                     A place for you to brainstorm new ideas. Kanban style.
                 </p>
                 </body>
             </div>
             <div className="about-me">
-                <p className="about-me header">About Me</p>
-                <div className="description-text">
+                <p className="name">About Me</p>
+                <div className="description-text less-empahsis">
                     <p>
-                        Currently, I am a final-year honours student at Victoria University of Wellington. I specialise in
+                        Currently, I am a final-year honours student at Victoria University of Wellington. I specialise
+                        in
                         Software Engineering and have particular passion for Cybersecurity.
                     </p>
                     <p>
-                        I think that the best ideas are inspired by the things around us - the people around us, the nature
+                        I think that the best ideas are inspired by the things around us - the people around us, the
+                        nature
                         around us. To me, the best ideas are the simplest and most usable be everyone.
                     </p>
                     <p>
-                        When I'm not online, I enjoy baking, 
+                        In my spare time I enjoy baking, tramping, and aeroplanes.
                     </p>
                 </div>
             </div>
